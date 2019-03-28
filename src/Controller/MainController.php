cbcc90ade7a3a1f2d54d4c640348d85985c91fd0
christophegourmand <christophe.gourmand@gmail.com>
2019-03-29 00:24:27 +0100

    fosuser works. I m now overriding templates

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('layout.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/moncompte", name="page_moncompte")
+     */
+    public function moncompte()
+    {
+        return $this->render('layout_extends/user/layout_ext_myaccount.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
